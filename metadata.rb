b90554846825432de0f6c6f3700d94dbf779bac8
Kirill Kouznetsov <agon.smith@gmail.com>
2014-07-01 15:47:40 +0400

    Metadata RuboCop syntax fix.

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,10 @@
-name             "newrelic"
-maintainer       "Kirill Kouznetsov"
-maintainer_email "agon.smith@gmail.com"
-license          "Apache 2.0"
-description      "Installs and configures Newrelic system monitor daemon"
+name             'newrelic'
+maintainer       'Kirill Kouznetsov'
+maintainer_email 'agon.smith@gmail.com'
+license          'Apache 2.0'
+description      'Installs and configures Newrelic system monitor daemon'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.3"
+version          '0.1.3'
 
 supports 'debian'
 supports 'ubuntu'

c2ef09bc6ee7323f850df6145c36aec83addc7eb
Kirill Kouznetsov <agon.smith@gmail.com>
2015-10-21 19:29:24 +0300

    Version bump: 0.1.7
    with some new dependencies.

--- metadata.rb
+++ metadata.rb
@@ -4,9 +4,12 @@ maintainer_email 'agon.smith@gmail.com'
 license          'Apache 2.0'
 description      'Installs and configures Newrelic system monitor daemon'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.6'
+version          '0.1.7'
 
 supports 'debian'
 supports 'ubuntu'
 
 depends 'apt'
+depends 'ruby_rbenv'
+depends 'runit'
+depends 'sudo'

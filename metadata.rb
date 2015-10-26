72ccafa2856158ea2371ff03be0387f7e7d8a2b5
Kirill Kouznetsov <agon.smith@gmail.com>
2015-10-26 12:59:01 +0300

    Version bump: 0.1.10

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'agon.smith@gmail.com'
 license          'Apache 2.0'
 description      'Installs and configures Newrelic system monitor daemon'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.9'
+version          '0.1.10'
 
 supports 'debian'
 supports 'ubuntu'
